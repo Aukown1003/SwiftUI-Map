2f15bfd63906c46580aeb225239feaabc689f45d
Auk1003 <Aukown1003@users.noreply.github.com>
2023-03-13 20:08:10 +0900

    Initial Commit

---
+++ SwiftUI-Map/SwiftUI_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapApp.swift
+//  SwiftUI-Map
+//
+//  Created by 桑野友輔 on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
