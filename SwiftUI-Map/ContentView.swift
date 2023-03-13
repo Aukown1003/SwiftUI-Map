662d376c77fd2ef3e7309b25edefba8a9ca07e23
Auk1003 <Aukown1003@users.noreply.github.com>
2023-03-13 21:24:46 +0900

    add map and pin

--- SwiftUI-Map/ContentView.swift
+++ SwiftUI-Map/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            MapView(searchKey: "羽田空港")
         }
-        .padding()
     }
 }
 
--- SwiftUI-Map/MapView.swift
+++ SwiftUI-Map/MapView.swift
@@ -6,15 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    let searchKey: String
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        print("検索キーワード:\(searchKey)")
+        
+        let geocoder = CLGeocoder()
+        
+        
+        geocoder.geocodeAddressString(
+            searchKey,
+            //completionHandler以降がクロージャ、特定の条件が満たされたときに実行
+            completionHandler: { (placemarks, error) in
+                if let placemarks,
+                   let firstPlacemark = placemarks.first,
+                   let location = firstPlacemark.location {
+                
+                    let targetCoordinate = location.coordinate
+                    
+                    print("緯度経度：\(targetCoordinate)")
+                    
+                    //ピンの作成
+                    let pin = MKPointAnnotation()
+                    
+                    pin.coordinate = targetCoordinate
+                    
+                    pin.title = searchKey
+                    
+                    uiView.addAnnotation(pin)
+                    uiView.region = MKCoordinateRegion(
+                        center: targetCoordinate,
+                        latitudinalMeters: 500.0,
+                        longitudinalMeters: 500.0
+                    )
+                }
+            })
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(searchKey: "羽田空港")
     }
 }
